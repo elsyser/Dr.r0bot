56740944858e9d1f223ad6d9eb41437e52441a03
Georgi Lyubenov <joro2404@abv.bg>
2019-02-16 00:28:27 +0200

    links fixed

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
-'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
   });
+  chrome.tabs.getSelected(null,function(tab) {
+    var tablink = tab.url;
+  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        //pageUrl: {hostEquals: "facebook.com"},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
-'use strict';
-
 let changeColor = document.getElementById('changeColor');
+
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
