519d8ff7ad9dd8eb16d12da9b22709e43f90ba16
Georgi Lyubenov <joro2404@abv.bg>
2019-02-16 10:34:56 +0200

    equal versions

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
-
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        //pageUrl: {hostEquals: "facebook.com"},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log('The color is green.');
+    });
+    chrome.tabs.getSelected(null,function(tab) {
+        var tablink = tab.url;
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                //pageUrl: {hostEquals: "facebook.com"},
+            })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
