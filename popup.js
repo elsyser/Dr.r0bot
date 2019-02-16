579aac3870ed93b2a4c9239ef9afd7d39123c712
Martin Jordanov <xxm0703@abv.bg>
2019-02-16 22:24:57 +0200

    JQuery try for saving popup state

--- popup.js
+++ popup.js
@@ -1,13 +1,35 @@
-let sslCheck = document.getElementById("ssl");
-let passCheck = document.getElementById("pass");
-let wifiCheck = document.getElementById("wifi");
+// sslCheck.onclick = () => { alert(ssl) }
+// passCheck.onclick = () => { alert(pass) }
+// wifiCheck.onclick = () => { alert(wifi) }
 
-export let ssl, pass, wifi;
+$(document).ready(function () {
+	// let sslCheck = document.getElementById("ssl");
+  // let passCheck = document.getElementById("pass");
+  // let wifiCheck = document.getElementById("wifi");
 
-// sslCheck.onclick = () => ssl = sslCheck.checked
-// passCheck.onclick = () => pass = passCheck.checked
-// wifiCheck.onclick = () => wifi = wifiCheck.checked
-window.onload = () => { alert(sslCheck) }
-sslCheck.onclick = () => { alert(ssl) }
-passCheck.onclick = () => { alert(pass) }
-wifiCheck.onclick = () => { alert(wifi) }
+	let sslValue = chrome.storage.local.get(["ssl"]);
+	let passValue = chrome.storage.local.get(["pass"]);
+	let wifiValue = chrome.storage.local.get(["wifi"]);
+
+	$('#ssl').checked = sslValue;
+	$('#pass').checked = passValue;
+	$('#wifi').checked = wifiValue;
+
+	$('#ssl').click(function () {
+		sslValue += 1;
+		sslValue %= 2;
+		chrome.storage.local.set({'ssl': sslValue}, null);
+	});
+
+	$('#pass').click(function () {
+		passValue += 1;
+		passValue %= 2;
+		chrome.storage.local.set({'pass': passValue}, null);
+	});
+
+	$('#wifi').click(function () {
+		wifiValue += 1;
+		wifiValue %= 2;
+		chrome.storage.local.set({'wifi': wifiValue}, null);
+	});
+});
